68bd9f48b6f9397e530522e0282f31fe190dd2b1
Ian Coulter <i.coulter1@hep.upenn.edu>
2017-05-12 13:29:14 -0400

    missed one

--- new_tubii_server/src/tubiiELLIE.h
+++ new_tubii_server/src/tubiiELLIE.h
@@ -57,10 +57,8 @@ double GetRate(void* MappedBaseAddress)
 {
   u32 period= mReadReg((u32) MappedBaseAddress, RegOffset1);
   float rate= HunMHz/period;
-  Log(WARNING,"rate-1 %f\n",rate);
   if(period==0) rate=0;
 
-  Log(WARNING,"rate-2 %f\n",rate);
   return rate;
 }
 

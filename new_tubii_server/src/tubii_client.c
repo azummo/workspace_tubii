192e55539f14a9302200566cb091d0bbc43d35b8
Ian Coulter <i.coulter1@hep.upenn.edu>
2017-05-12 13:33:38 -0400

    Tidy

--- new_tubii_server/src/tubii_client.c
+++ new_tubii_server/src/tubii_client.c
@@ -1237,9 +1237,6 @@ static int save_tubii(aeEventLoop *el, long long id, void *data)
                      );
 
     Log(WARNING, "---- %s\n", command);
-    Log(WARNING, "----\n");
-    Log(WARNING, "----\n");
-    Log(WARNING, "----\n");
 
     if (db_exec_async(detector_db, command, save_db_callback, NULL)) {
         Log(WARNING, "database isn't connected to save tubii state");

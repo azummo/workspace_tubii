73924b56094eddd33ec83ff31960e7153ad3caf5
Ian Coulter <i.coulter1@hep.upenn.edu>
2017-05-12 13:07:03 -0400

    fuck off

--- new_tubii_server/src/tubii_client.c
+++ new_tubii_server/src/tubii_client.c
@@ -1236,7 +1236,7 @@ static int save_tubii(aeEventLoop *el, long long id, void *data)
                      GetNPulses(MappedPulserBaseAddress),GetDelayLength(MappedDelayBaseAddress)
                      );
 
-    Log(WARNING, "%s\n", command);
+    Log(WARNING, "---- %s\n", command);
 
     if (db_exec_async(detector_db, command, save_db_callback, NULL)) {
         Log(WARNING, "database isn't connected to save tubii state");
